24883a642b7c6e2ad58439477c983782081828f7
Grigor Yeghiazaryan <yeghiazaryan@gmail.com>
2013-08-03 18:53:54 +0400

    Basic transliteration

--- js/armslit.js
+++ js/armslit.js
@@ -1,6 +1,27 @@
+var a = {'ա':'a','բ':'b','գ':'g','դ':'d','ե':'e','զ':'z','է':'e','ը':'y','թ':'th','ժ':'zh','ի':'i','լ':'l','խ':'kh','ծ':'ts','կ':'k','հ':'h','ձ':'dz','ղ':'gh','ճ':'tch','մ':'m','յ':'y','ն':'n','շ':'sh','ո':'o','չ':'ch','պ':'p','ջ':'dzh','ռ':'r','ս':'s','վ':'v','տ':'t','ր':'r','ց':'c','ու':'u','փ':'ph','ք':'q','և':'ev','օ':'o','ֆ':'f',
+         'Ա':'A','Բ':'B','Գ':'G','Դ':'D','Ե':'E','Զ':'Z','Է':'E','Ը':'Y','Թ':'Th','Ժ':'Zh','Ի':'I','Լ':'L','Խ':'Kh','Ծ':'Ts','Կ':'K','Հ':'H','Ձ':'Dz','Ղ':'Gh','Ճ':'Tch','Մ':'M','Յ':'Y','Ն':'N','Շ':'Sh','Ո':'Vo','Չ':'Ch','Պ':'P','Ջ':'Dzh','Ռ':'R','Ս':'S','Վ':'V','Տ':'T','Ր':'R','Ց':'C','Ու':'U','Փ':'Ph','Ք':'Q','Օ':'O','Ֆ':'F'};
+
+function transliterate(word, dict){
+	return word.split('').map(function (char) {
+    	return dict[char] || char; 
+  	}).join("");
+}
+
+function invert(obj) {
+
+  var new_obj = {};
+
+  for (var prop in obj) {
+    if(obj.hasOwnProperty(prop)) {
+      new_obj[obj[prop]] = prop;
+    }
+  }
+
+  return new_obj;
+};
+
 function convert(s) {
-	// ա բ գ դ ե զ է ը թ ժ ի լ խ ծ կ հ ձ ղ ճ մ յ ն շ ո չ պ ջ ռ ս վ տ ր ց ու փ ք և օ ֆ
-	return [s];
+	return [s, transliterate(s, invert(a))];
 }
 
 function showList(list) {
